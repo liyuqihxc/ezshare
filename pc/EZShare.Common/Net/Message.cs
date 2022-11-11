ddea452437852ece793ad1daf86f619936314815
liyuqihxc <liyuqihxc@gmail.com>
2022-11-11 14:35:58 +0800

    network package

---
+++ pc/EZShare.Common/Net/Message.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace EZShare.Common.Net
+{
+    public static class ContentTypes
+    {
+        public static string Json => "json";
+
+        public static string ByteStream => "bytestream";
+
+        public static string Text => "text";
+    }
+
+    public enum TransferCompressionTypes
+    {
+        Lzma,
+        gzip
+    }
+
+    public class RemoteMessage
+    {
+        public RemoteMessage()
+        {
+
+        }
+
+        public int ContentLength { get; }
+
+        public string ContentType { get; }
+
+        public string? SessionId { get; }
+
+        public bool? TransferEncodingChunked { get; }
+
+        public TransferCompressionTypes? TransferCompressionType { get; }
+
+        public string? ContentPathName { get; }
+
+        public Stream Content { get; }
+    }
+
+    public static class MessageExtensions
+    {
+        public static Task<RemoteMessage> GetRemoteMessage(this Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<RemoteMessage> GetEncryptedRemoteMessage(this Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<T> ReadContentAsJsonAsync<T>(this RemoteMessage remoteMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<string> ReadContentAsText(this RemoteMessage remoteMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
